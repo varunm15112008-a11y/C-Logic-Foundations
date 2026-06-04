420d194ef072da1f99b119d808580c33fbcd327f
Varun M <varun.m15112008@gmail.com>
2026-06-04 08:24:14 +0530

    Added Type 1 Function (Takes nothing, returns nothing) - Factory Boot Protocol

---
+++ system_powering.C
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+void bootSequence()
+  {
+    printf("system powering up...\n");
+    printf("all engines online.\n");
+  }
+
+int main()
+  {
+    bootSequence();
+
+    return 0;
+  }
